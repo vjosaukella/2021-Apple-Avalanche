63b9486e4c8556a2a2c1507b311a265e4d196de8
Mr. Kiedes <akiedes@vtsd.com>
2021-11-01 12:37:41 -0400

    Initial Update

---
+++ main.py
@@ -0,0 +1,23 @@
+#   a123_apple_1.py
+import turtle as trtl
+
+#-----setup-----
+apple_image = "apple.gif" # Store the file name of your shape
+
+wn = trtl.Screen()
+wn.setup(width=1.0, height=1.0)
+wn.addshape(apple_image) # Make the screen aware of the new file
+
+apple = trtl.Turtle()
+
+#-----functions-----
+# given a turtle, set that turtle to be shaped by the image file
+def draw_apple(active_apple):
+  active_apple.shape(apple_image)
+  wn.update()
+
+
+#-----function calls-----
+draw_apple(apple)
+
+wn.mainloop()
